2b7865ad2391d0850ae8958188937405525d542f
unknown <swetha@pipli.ai>
2023-02-13 16:24:18 +0530

    updated code

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express');
-const axios = require('axios');
+const request = require("request-promise");
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-ip");
-  next();
-});
 
-app.get('/lookup-location', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    const clientIp = req.headers['x-client-ip'];
-    const response = await axios.get(`https://api.ipdata.co/${clientIp}?api-key=e52b05757ad1ed4a66cc621d9e09af5f33256b8273fc9421805b5bc3`);
-    const locationData = response.data;
-    console.log(locationData);
-   return  res.json(locationData);
-  } catch (error) {
-   return res.status(500).send({
-      message: 'Error looking up location',
-      err: error
+    const url = "http://ip-api.com/json";
+    const response = await request(url);
+    console.log(response);
+    const data = JSON.parse(response);
+    const clientIp = data.query;
+    const locationData = await request(`https://api.ipdata.co/${clientIp}?api-key=e52b05757ad1ed4a66cc621d9e09af5f33256b8273fc9421805b5bc3`);
+    const resp =JSON.parse(locationData);
+    res.send({
+      IP:resp.ip,
+      City:res.city,
+      Region:resp.region,
+      Country:resp.country_name,
+      Postal:resp.postal
     });
+  } catch (error) {
+    res.send("Error!");
   }
-
 });
 
-app.listen(8000, () => console.log('Listening on port 8000'));
+
+app.listen(8000, () => console.log('Listening on port 8000'));  
